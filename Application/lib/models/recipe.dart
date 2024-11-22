1c9fe1b8eb2593e80efc327ddab3eaacb03808ad
afrinatabassum <141935693+afrinatabassum@users.noreply.github.com>
2024-11-22 19:48:37 +0600

    Add files via upload

---
+++ Application/lib/models/recipe.dart
@@ -0,0 +1,13 @@
+class Recipe {
+  final String id;
+  final String name;
+  final List<String> ingredients;
+  final String instructions;
+
+  Recipe({
+    required this.id,
+    required this.name,
+    required this.ingredients,
+    required this.instructions,
+  });
+}
